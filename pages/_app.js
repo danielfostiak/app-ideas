d55a37a428a28ed2e48acbac3735c5f230e20cb3
Daniel <fostiakd@gmail.com>
2023-01-03 20:30:21 +0000

    bruh

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,17 @@ import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 import { UserContext } from "../lib/context";
 import { useUserData } from "../lib/hooks";
+import { Theme } from "react-daisyui";
 
 export default function App({ Component, pageProps }) {
   const userData = useUserData();
 
   return (
     <UserContext.Provider value={userData}>
-      <Navbar />
-      <Component {...pageProps} />
+      <Theme dataTheme="dracula">
+        <Navbar />
+        <Component {...pageProps} />
+      </Theme>
     </UserContext.Provider>
   );
 }
