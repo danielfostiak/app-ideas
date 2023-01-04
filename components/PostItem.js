9e5e2ad098f2b53065f200a1e292dfa86f519b0c
Daniel <fostiakd@gmail.com>
2023-01-04 18:10:40 +0000

    styled postitem

--- components/PostItem.js
+++ components/PostItem.js
@@ -4,8 +4,10 @@ export default function PostItem({ author, title, content, createdAt, id }) {
   return (
     <div className="card bg-background shadow-md m-2">
       <div className="card-body">
-        <h2 className="card-title text-foreground">{title}</h2>
-        <p className="text-foreground">{content}</p>
+        <h2 className="card-title bg-comment rounded-md w-fit px-3 py-2 text-foreground">
+          {title}
+        </h2>
+        <p className="text-foreground ">{content}</p>
         <div className="card-actions justify-end">
           <button className="group relative justify-center rounded-md border border-transparent bg-green py-2 px-4 text-sm font-medium text-background hover:bg-darkergreen ">
             <Link href={`/${id}`} className="font-bold">
