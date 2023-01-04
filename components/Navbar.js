422dd7df1e09bbc23e3a64a2da6136c9c748325a
Daniel <fostiakd@gmail.com>
2023-01-04 02:03:58 +0000

    push to main page after signout

--- components/Navbar.js
+++ components/Navbar.js
@@ -13,7 +13,8 @@ function classNames(...classes) {
 
 export default function Navbar() {
   const { user, username } = useContext(UserContext);
-  const { asPath } = useRouter();
+  const router = useRouter();
+  const { asPath } = router;
   const navigation = [
     { name: "Home", href: "/" },
     { name: "Explore", href: "/explore" },
@@ -150,7 +151,12 @@ export default function Navbar() {
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700"
                             )}
-                            onClick={() => auth.signOut()}
+                            onClick={() => {
+                              router.push({
+                                pathname: "/",
+                              });
+                              auth.signOut();
+                            }}
                           >
                             Sign out
                           </Link>
