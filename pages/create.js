0951dd85a0531ae3ff091c1567b2e27e2157e3f4
Daniel <fostiakd@gmail.com>
2023-01-27 17:33:33 +0000

    errors in posting

--- pages/create.js
+++ pages/create.js
@@ -1,29 +1,34 @@
 import CreateIdea from "../components/CreateIdea";
 import { firestore, fromMillis } from "../lib/firebase";
 import { UserContext } from "../lib/context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 export default function Create() {
   const { user, username } = useContext(UserContext);
+  const [posted, setPosted] = useState(false);
 
   const onSubmit = async (title, description, content) => {
     if (!title || !description || !content) {
       alert("Fill out form fully");
       return;
     }
-    const id = title.replace(/\s+/g, "-").toLowerCase();
-    const ideasRef = firestore.doc(`users/${user.uid}`).collection("ideas");
+    try {
+      const id = title.replace(/\s+/g, "-").toLowerCase();
+      const ideasRef = firestore.doc(`users/${user.uid}`).collection("ideas");
 
-    await ideasRef.doc(id).set({
-      title,
-      description,
-      content,
-      id,
-      username,
-      createdAt: fromMillis(new Date().getTime()),
-    });
-
-    // postIdea(title, description, content);
+      await ideasRef.doc(id).set({
+        title,
+        description,
+        content,
+        id,
+        username,
+        createdAt: fromMillis(new Date().getTime()),
+      });
+      setPosted("posted");
+    } catch (error) {
+      console.log(error);
+      setPosted("error");
+    }
   };
 
   return (
@@ -31,7 +36,23 @@ export default function Create() {
       <h1 className="mb-4 text-4xl font-extrabold text-center leading-none tracking-tight text-background md:text-5xl lg:text-6xl dark:text-white">
         Create Idea
       </h1>
-      <CreateIdea handleSubmit={onSubmit} />
+      {posted ? (
+        <Success posted={posted} />
+      ) : (
+        <CreateIdea handleSubmit={onSubmit} />
+      )}
     </>
+  );
+}
+
+function Success({ posted }) {
+  return posted === "posted" ? (
+    <h1 className="mb-4 text-4xl font-extrabold text-center leading-none tracking-tight text-background md:text-5xl lg:text-6xl dark:text-white">
+      Successfully posted
+    </h1>
+  ) : (
+    <h1 className="mb-4 text-4xl font-extrabold text-center leading-none tracking-tight text-background md:text-5xl lg:text-6xl dark:text-white">
+      Error in posting, please try again
+    </h1>
   );
 }
